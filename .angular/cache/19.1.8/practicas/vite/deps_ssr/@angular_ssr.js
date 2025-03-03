788d5ab0cc3487130ab333b8118659e621d34094
ChrisPau0530 <230809@utxicotepec.edu.mx>
2025-03-03 14:28:56 -0600

    Practica 07
    Aun no renderiza

--- .angular/cache/19.1.8/practicas/vite/deps_ssr/@angular_ssr.js
+++ .angular/cache/19.1.8/practicas/vite/deps_ssr/@angular_ssr.js
@@ -14,9 +14,9 @@ import {
   setAngularAppEngineManifest,
   setAngularAppManifest,
   withAppShell
-} from "./chunk-X23RXUC2.js";
-import "./chunk-M2K263XA.js";
+} from "./chunk-PTKWDAZR.js";
 import "./chunk-374UCYDV.js";
+import "./chunk-M2K263XA.js";
 import "./chunk-GWOHOPUO.js";
 import "./chunk-C5KUQ6XX.js";
 import "./chunk-Z5IWENK5.js";
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PageContainerComponent } from "./components/page-container/page-container.component";
 import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb.component";
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
